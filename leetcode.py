bfdfdf32d33c67746e79f62701b730c227b91fbe
notdien <thedien7000@gmail.com>
2023-09-20 13:56:41 -0400

    69

--- leetcode.py
+++ leetcode.py
@@ -3,33 +3,36 @@ class Solution():
         pass
 
     def solved(self, x):
-        # if it is negative, just return
-        if x < 0:
-            return
-
         # initiate a binary search
         low = 1
         high = x
-        sqrt = 0
+
+        # we need a variable to store our return value
+        sqrt = x
 
         while low <= high:
-            mid = x // 2
+            # if we just do low+high/2, the low+high = 3,000,000,001, which will result in overflow error
+            mid = int(low + (high-low)/2)
+            print(mid)
 
-            # compare the given x with the mid * mid
-            compare = mid * mid
-            # if compare is higher, go lower
+            # if we get a case of 2x2 = 4, then we just return 2
+            if mid*mid == x:
+                return mid
+            # if comparison is higher, go lower
             # we go towards the left side
-            if compare > high:
+            elif mid*mid > x:
                 high = mid - 1
-
-            # if it is smaller then it could potientally be the square root
+            # if it is comparison is smaller, it might be the correct answer
             else:
-                sqrt = compare
-                # we also have to check if there is any larger number that is smaller than x
+                sqrt = mid
+                # checks to make sure if there are no numbers bigger that could be sqrt root
+                # if there is - repeats loop
+                # otherwise we just return sqrt
+                low = mid + 1
 
         return sqrt
 
 
 # initiate a object
 result = Solution()
-print(result.solved(15))
+print(result.solved(9))
